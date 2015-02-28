1a36c30885c3f2dbcd562d8a318bc6f8439a36e5
Ivo Ugrina <ivo@iugrina.com>
2015-02-28 11:21:43 +0100

    removed some obsolete code from plasma.hplc.derived.traits

--- R/derived.traits.R
+++ R/derived.traits.R
@@ -100,26 +100,26 @@ plasma.hplc.derived.traits <- function(d) {
 	d = d[, c(1:(grep("GP1$", names(d))-1), grep("GP\\d+$|DG\\d+$|sial",names(d)))]
 	
 	# sialylation of biantennary glycans 
-	d$`BAMS`= with(d, (GP7 + GP8)/(DG5 + DG6 + DG7), digits = 4)*100
-	d$`BADS`= with(d, (GP9 + GP10 + GP11)/(DG5 + DG6 + DG7), digits = 4)*100
+	d$`BAMS`= with(d, (GP7 + GP8)/(DG5 + DG6 + DG7))*100
+	d$`BADS`= with(d, (GP9 + GP10 + GP11)/(DG5 + DG6 + DG7))*100
 	
 	# branching
-	d$`BA`= with(d, (DG1 + DG2 + DG3 + DG4 + DG5 + DG6 + DG7), digits = 2)
-	d$`TRIA`= with(d, (DG8 + DG9 + DG10), digits = 2)
-	d$`TA`= with(d, (DG11 + DG12 + DG13), digits = 2)
+	d$`BA`= with(d, (DG1 + DG2 + DG3 + DG4 + DG5 + DG6 + DG7))
+	d$`TRIA`= with(d, (DG8 + DG9 + DG10))
+	d$`TA`= with(d, (DG11 + DG12 + DG13))
 	
 	# fucosylation
-	d$`C-FUC`= with(d, DG6/(DG5 + DG6), digits = 4)*100
-	d$`A-FUC`= with(d, DG7/(DG5 + DG7), digits = 4)*100
+	d$`C-FUC`= with(d, DG6/(DG5 + DG6))*100
+	d$`A-FUC`= with(d, DG7/(DG5 + DG7))*100
 	
-	d$`A2`= with(d, (GP1+DG1)/2, digits = 4)
+	d$`A2`= with(d, (GP1+DG1)/2)
 	
 	# galactosylation
-	d$`G0`= with(d, (DG1 + DG2), digits = 2)
-	d$`G1`= with(d, (DG3 + DG4), digits = 2)
-	d$`G2`= with(d, (DG5 + DG6 + DG7), digits = 2)
-	d$`G3`= with(d, (GP12 + GP13 + GP14), digits = 2)
-	d$`G4`= with(d, (GP15 + GP16), digits = 2)
+	d$`G0`= with(d, (DG1 + DG2))
+	d$`G1`= with(d, (DG3 + DG4))
+	d$`G2`= with(d, (DG5 + DG6 + DG7))
+	d$`G3`= with(d, (GP12 + GP13 + GP14))
+	d$`G4`= with(d, (GP15 + GP16))
 	
 	return(d)
 }
