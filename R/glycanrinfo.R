30b77c4167b56c4930fc5e6f9ae1f635184713ef
Ivo Ugrina <ivo@iugrina.com>
2015-11-25 16:12:07 +0000

    add example data for IgG(UPLC)

---
+++ R/glycanrinfo.R
@@ -0,0 +1,16 @@
+
+#
+# Data section
+#
+
+#' Multiple plates IgG UPLC data example
+#' 
+#' This is an example of the data obtained by UPLC
+#' while analysing IgG.
+"mpiu"
+
+#' Normalizied mpiu data
+#' 
+#' This is an example of the normalized mpiu data.
+#' It is intended to be used for unit tests.
+"mpiunorm"
